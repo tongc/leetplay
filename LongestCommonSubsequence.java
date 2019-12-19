b15445d575345f0d7525007e2cdd7d9a48245993
Tong Chen <chentongtc@gmail.com>
2019-12-19 21:25:19 +0000

    longest sub sequence

--- LongestCommonSubsequence.java
+++ LongestCommonSubsequence.java
@@ -37,6 +37,24 @@ public class LongestCommonSubsequence {
         return results;
     }
 
+    public String lcs(List<String> first, List<String> second) {
+        if(first.size() == 0 || second.size() == 0) {
+            return "";
+        }
+        String firstLetter = first.get(0);
+        String secondLetter = second.get(0);
+        if(firstLetter.equals(secondLetter)) {
+            first.remove(0);
+            second.remove(0);
+            return lcs(first, second) + firstLetter;
+        } else if(first.size() == 1 || second.size() == 1) {
+            return "";
+        }
+        String left = lcs(first.subList(1, first.size()), second);
+        String right = lcs(first, second.subList(1, second.size()));
+        return left.length()>right.length()?left:right;
+    }
+
     public static void main(String[] args) {
         LongestCommonSubsequence lcs = new LongestCommonSubsequence();
         List<String> abcdef = lcs.allSubsequences("abcdef");
@@ -45,5 +63,23 @@ public class LongestCommonSubsequence {
         System.out.println(base2Enum + " size: " + base2Enum.size());
         List<String> base10Enum = lcs.enumerateBase2(3, 10);
         System.out.println(base10Enum + " size: " + base10Enum.size());
+
+        List<String> first = new ArrayList<>();
+        first.add("a");
+        first.add("b");
+        first.add("c");
+        first.add("d");
+        first.add("f");
+        first.add("g");
+        first.add("g");
+
+        List<String> second = new ArrayList<>();
+        second.add("b");
+        second.add("c");
+        second.add("d");
+        second.add("f");
+        second.add("g");
+        second.add("h");
+        System.out.println(lcs.lcs(first, second));
     }
 }
